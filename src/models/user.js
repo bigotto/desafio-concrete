eabb17da487fa80bed97ba6cdf5ec5cbfa265eb1
Bruno Bigotto <brunobigotto@hotmail.com>
2020-01-24 14:11:03 -0400

    Correção da fórmula da diferença dos minutos e validação no campo de email

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 const bycript = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
@@ -14,6 +15,11 @@ const userSchema = new mongoose.Schema({
         unique: true,
         required: true,
         lowercase: true,
+        validate(value) {
+            if (!validator.isEmail(value)) {
+                throw new Error('Email inválido')
+            }
+        }
     },
     senha: {
         type: String,
